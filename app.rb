8ca7218c802381bcbdc993ab41a065a6757c557a
Frankie Roberto <frankie@frankieroberto.com>
2020-09-14 22:30:07 +0100

    initial test

---
+++ app.rb
@@ -0,0 +1,64 @@
+require "mini_magick"
+require 'json'
+
+class PdfFile
+
+  attr_reader :file_name, :page
+
+  def initialize(file_name, page)
+    @file_name = file_name
+    @page = page
+  end
+
+  def type
+    file.type
+  end
+
+  private
+
+  def file
+    url = "#{ENV.fetch('ORIGIN_URL')}/#{file_name}/#{page}"
+    puts url
+    @file ||= MiniMagick::Image.open("#{ENV.fetch('ORIGIN_URL')}/#{file_name}/#{page}")
+    # @file.resize "100x100"
+  end
+
+end
+
+
+class PdfRenderApp
+
+  REGEX = /\A\/([^\/]+)\/((?:\d+|info))\z/
+
+  def call(env)
+
+    req = Rack::Request.new(env)
+
+    puts req.path_info
+
+    path_match = req.path_info.match(REGEX)
+
+    if path_match
+
+      file_name = path_match[1]
+      page = path_match[2]
+
+      file = PdfFile.new(file_name, page)
+
+      [
+        200,
+        {"Content-Type" => "text/plain"}, [file.type]
+      ]
+
+    else
+
+      [
+        404,
+        {"Content-Type" => "text/plain"}, ["Not found"]
+      ]
+
+    end
+
+  end
+
+end
